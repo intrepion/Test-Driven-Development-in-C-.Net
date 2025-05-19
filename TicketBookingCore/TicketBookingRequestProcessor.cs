4f3c09d3439932909cc93ff4dd54d4c9216174e2
Oliver Forral <intrepion@gmail.com>
2025-05-18 23:31:44 -0600

    Writing code to make the test pass

--- TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -2,8 +2,11 @@ namespace TicketBookingCore;
 
 public class TicketBookingRequestProcessor
 {
+    private readonly ITicketBookingRepository _ticketBookingRepository;
+
     public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
     {
+        _ticketBookingRepository = ticketBookingRepository;
     }
 
     public TicketBookingResponse Book(TicketBookingRequest request)
@@ -12,6 +15,13 @@ public class TicketBookingRequestProcessor
         {
             throw new ArgumentNullException(nameof(request));
         }
+
+        _ticketBookingRepository.Save(new TicketBooking
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email
+        });
 
         return new TicketBookingResponse
         {
